b37a91e875c882905d54e2b87aa126f38b57f246
Sébastien Mosser <sebastien.mosser@gmail.com>
2019-03-27 15:30:48 -0400

    final version

--- src/main/java/tests/IntegerTests.java
+++ src/main/java/tests/IntegerTests.java
@@ -10,7 +10,7 @@ public class IntegerTests extends TestCase {
 
     private int x, y = 0;
 
-    @Override public void setUp() { x = 1; y = 1; }
+    public void setUp() { x = 1; y = 1; }
     public void test_addTwoNumbers() { assertEquals(2, x+y); }
     public void test_subtractTwoNumbers() { assertEquals(1, x-y); }
     public void test_throwAnException() { throw new RuntimeException(); }
